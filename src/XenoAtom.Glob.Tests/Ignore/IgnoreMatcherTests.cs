ffa99ed42e9464be5f12921b2e6944c123bebed9
Alexandre Mutel <alexandre_mutel@live.com>
2026-04-04 15:03:51 +0200

    Expand ignore matcher edge-case tests

--- src/XenoAtom.Glob.Tests/Ignore/IgnoreMatcherTests.cs
+++ src/XenoAtom.Glob.Tests/Ignore/IgnoreMatcherTests.cs
@@ -92,6 +92,20 @@ public class IgnoreMatcherTests
     }
 
     [TestMethod]
+    public void Evaluate_ShouldNotTreatBasenameRulesAsPrefixMatches()
+    {
+        var matcher = new IgnoreMatcher(IgnoreRuleSet.ParseGitIgnore("vendor"));
+
+        var exactResult = matcher.Evaluate("src/vendor/file.txt");
+        var prefixedResult = matcher.Evaluate("src/vendors/file.txt");
+        var suffixedResult = matcher.Evaluate("src/myvendor/file.txt");
+
+        Assert.IsTrue(exactResult.IsIgnored);
+        Assert.IsFalse(prefixedResult.IsMatch);
+        Assert.IsFalse(suffixedResult.IsMatch);
+    }
+
+    [TestMethod]
     public void Evaluate_ShouldPreservePrecedenceForIndexedBasenameRules()
     {
         var rules = Enumerable.Range(0, 40)
@@ -162,6 +176,44 @@ public class IgnoreMatcherTests
     }
 
     [TestMethod]
+    public void Evaluate_ShouldAllowNegatedDirectoryUnderSingleStarPatternToKeepChildrenReachable()
+    {
+        var matcher = new IgnoreMatcher(IgnoreRuleSet.ParseGitIgnore("""
+            foo/*
+            !foo/bar
+            """));
+
+        var fooDirectory = matcher.Evaluate("foo", isDirectory: true);
+        var barDirectory = matcher.Evaluate("foo/bar", isDirectory: true);
+        var barChild = matcher.Evaluate("foo/bar/baz.txt");
+        var sibling = matcher.Evaluate("foo/other.txt");
+
+        Assert.IsFalse(fooDirectory.IsIgnored);
+        Assert.IsTrue(barDirectory.IsMatch);
+        Assert.IsFalse(barDirectory.IsIgnored);
+        Assert.AreEqual("!foo/bar", barDirectory.Rule!.RawPatternText);
+        Assert.IsFalse(barChild.IsIgnored);
+        Assert.IsTrue(sibling.IsIgnored);
+    }
+
+    [TestMethod]
+    public void Evaluate_ShouldNotAllowNegatedDeepFileToEscapeIgnoredIntermediateDirectory()
+    {
+        var matcher = new IgnoreMatcher(IgnoreRuleSet.ParseGitIgnore("""
+            foo/*
+            !foo/bar/baz.txt
+            """));
+
+        var barDirectory = matcher.Evaluate("foo/bar", isDirectory: true);
+        var bazFile = matcher.Evaluate("foo/bar/baz.txt");
+
+        Assert.IsTrue(barDirectory.IsIgnored);
+        Assert.AreEqual("foo/*", barDirectory.Rule!.RawPatternText);
+        Assert.IsTrue(bazFile.IsIgnored);
+        Assert.AreEqual("foo/*", bazFile.Rule!.RawPatternText);
+    }
+
+    [TestMethod]
     public void Evaluate_ShouldNotTreatTrailingDoubleStarAsIgnoringTheDirectoryItself()
     {
         var matcher = new IgnoreMatcher(IgnoreRuleSet.ParseGitIgnore("""
@@ -182,6 +234,21 @@ public class IgnoreMatcherTests
     }
 
     [TestMethod]
+    public void Evaluate_ShouldTreatSingleStarChildPatternAsIgnoringImmediateChildrenOnly()
+    {
+        var matcher = new IgnoreMatcher(IgnoreRuleSet.ParseGitIgnore("foo/*"));
+
+        var fooDirectory = matcher.Evaluate("foo", isDirectory: true);
+        var childFile = matcher.Evaluate("foo/file.txt");
+        var grandChildFile = matcher.Evaluate("foo/bar/file.txt");
+
+        Assert.IsFalse(fooDirectory.IsIgnored);
+        Assert.IsTrue(childFile.IsIgnored);
+        Assert.IsTrue(grandChildFile.IsIgnored);
+        Assert.AreEqual("foo/*", grandChildFile.Rule!.RawPatternText);
+    }
+
+    [TestMethod]
     public void Evaluate_ShouldAllowNegatedDirectoryExceptionToKeepChildrenReachable()
     {
         var matcher = new IgnoreMatcher(IgnoreRuleSet.ParseGitIgnore("""
@@ -196,6 +263,21 @@ public class IgnoreMatcherTests
         Assert.IsFalse(directoryResult.IsIgnored);
         Assert.AreEqual("!src/coreclr/debug", directoryResult.Rule!.RawPatternText);
         Assert.IsFalse(childResult.IsIgnored);
+    }
+
+    [TestMethod]
+    public void Evaluate_ShouldPreferLaterRuleAfterNegation()
+    {
+        var matcher = new IgnoreMatcher(IgnoreRuleSet.ParseGitIgnore("""
+            *.tmp
+            !keep.tmp
+            keep.tmp
+            """));
+
+        var result = matcher.Evaluate("keep.tmp");
+
+        Assert.IsTrue(result.IsIgnored);
+        Assert.AreEqual("keep.tmp", result.Rule!.PatternText);
     }
 
     [TestMethod]
@@ -218,6 +300,20 @@ public class IgnoreMatcherTests
     }
 
     [TestMethod]
+    public void Evaluate_ShouldMatchRecursiveWildcardWithZeroOrMoreSegments()
+    {
+        var matcher = new IgnoreMatcher(IgnoreRuleSet.ParseGitIgnore("a/**/b.txt"));
+
+        var directChild = matcher.Evaluate("a/b.txt");
+        var nestedChild = matcher.Evaluate("a/x/y/b.txt");
+        var nonMatch = matcher.Evaluate("a/x/y/c.txt");
+
+        Assert.IsTrue(directChild.IsIgnored);
+        Assert.IsTrue(nestedChild.IsIgnored);
+        Assert.IsFalse(nonMatch.IsMatch);
+    }
+
+    [TestMethod]
     public void Evaluate_ShouldRespectRuleSetPrecedence()
     {
         var lowPrecedence = IgnoreRuleSet.ParseGitIgnore("*.log");
@@ -232,6 +328,22 @@ public class IgnoreMatcherTests
     }
 
     [TestMethod]
+    public void Evaluate_ShouldAllowHigherPrecedenceNestedRuleSetToOverrideRootRules()
+    {
+        var rootRules = IgnoreRuleSet.ParseGitIgnore("*.tmp");
+        var nestedRules = IgnoreRuleSet.ParseGitIgnore("!keep.tmp", baseDirectory: "src");
+        var matcher = new IgnoreMatcher(rootRules, nestedRules);
+
+        var nestedResult = matcher.Evaluate("src/keep.tmp");
+        var otherResult = matcher.Evaluate("other/keep.tmp");
+
+        Assert.IsTrue(nestedResult.IsMatch);
+        Assert.IsFalse(nestedResult.IsIgnored);
+        Assert.AreEqual("!keep.tmp", nestedResult.Rule!.RawPatternText);
+        Assert.IsTrue(otherResult.IsIgnored);
+    }
+
+    [TestMethod]
     public void ParseGitIgnore_ShouldPreserveEscapedTrailingSpaces()
     {
         var matcher = new IgnoreMatcher(IgnoreRuleSet.ParseGitIgnore("space\\ \n"));
@@ -239,6 +351,18 @@ public class IgnoreMatcherTests
         var result = matcher.Evaluate("space ");
 
         Assert.IsTrue(result.IsIgnored);
+    }
+
+    [TestMethod]
+    public void ParseGitIgnore_ShouldTrimUnescapedTrailingSpaces()
+    {
+        var matcher = new IgnoreMatcher(IgnoreRuleSet.ParseGitIgnore("trimmed.txt   \n"));
+
+        var trimmed = matcher.Evaluate("trimmed.txt");
+        var literalSpaces = matcher.Evaluate("trimmed.txt   ");
+
+        Assert.IsTrue(trimmed.IsIgnored);
+        Assert.IsFalse(literalSpaces.IsMatch);
     }
 
     [TestMethod]
@@ -256,6 +380,20 @@ public class IgnoreMatcherTests
     }
 
     [TestMethod]
+    public void ParseGitIgnore_ShouldTreatEscapedBangAsLiteral()
+    {
+        var matcher = new IgnoreMatcher(IgnoreRuleSet.ParseGitIgnore("\\!literal.txt\n"));
+
+        var ignored = matcher.Evaluate("!literal.txt");
+        var negatedLooking = matcher.Evaluate("literal.txt");
+
+        Assert.IsTrue(ignored.IsIgnored);
+        Assert.IsFalse(negatedLooking.IsMatch);
+        Assert.AreEqual("\\!literal.txt", ignored.Rule!.PatternText);
+        Assert.AreEqual("\\!literal.txt", ignored.Rule.RawPatternText);
+    }
+
+    [TestMethod]
     public void Evaluate_ShouldAnchorLeadingSlashPatternsToRuleBaseDirectory()
     {
         var matcher = new IgnoreMatcher(IgnoreRuleSet.ParseGitIgnore("/hello.*\n"));
@@ -265,6 +403,20 @@ public class IgnoreMatcherTests
 
         Assert.IsTrue(rootMatch.IsIgnored);
         Assert.IsFalse(nestedMatch.IsMatch);
+    }
+
+    [TestMethod]
+    public void Evaluate_ShouldAnchorLeadingSlashPatternsToNestedBaseDirectory()
+    {
+        var matcher = new IgnoreMatcher(IgnoreRuleSet.ParseGitIgnore("/generated/\n", baseDirectory: "src"));
+
+        var anchoredMatch = matcher.Evaluate("src/generated/file.cs");
+        var nestedNonMatch = matcher.Evaluate("src/deep/generated/file.cs");
+        var outsideBaseNonMatch = matcher.Evaluate("other/generated/file.cs");
+
+        Assert.IsTrue(anchoredMatch.IsIgnored);
+        Assert.IsFalse(nestedNonMatch.IsMatch);
+        Assert.IsFalse(outsideBaseNonMatch.IsMatch);
     }
 
     [TestMethod]
