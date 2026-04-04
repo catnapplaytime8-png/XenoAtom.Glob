67f396800321d1339b47f336386f555a078447e9
Alexandre Mutel <alexandre_mutel@live.com>
2026-04-04 16:01:55 +0200

    Add public API null guard tests

--- src/XenoAtom.Glob.Tests/PublicApiGuardTests.cs
+++ src/XenoAtom.Glob.Tests/PublicApiGuardTests.cs
@@ -18,9 +18,65 @@ public class PublicApiGuardTests
     }
 
     [TestMethod]
-    public void IgnoreRuleSet_ParseGitIgnore_ShouldThrowOnNull()
+    public void GlobPattern_TryParse_ShouldThrowOnNull()
+    {
+        Assert.Throws<ArgumentNullException>(() => GlobPattern.TryParse(null!));
+    }
+
+    [TestMethod]
+    public void GlobPattern_IsMatch_ShouldThrowOnNull()
+    {
+        var pattern = GlobPattern.Parse("*.tmp");
+        Assert.Throws<ArgumentNullException>(() => pattern.IsMatch((string)null!));
+    }
+
+    [TestMethod]
+    public void IgnoreRuleSet_ParseGitIgnore_String_ShouldThrowOnNull()
     {
         Assert.Throws<ArgumentNullException>(() => IgnoreRuleSet.ParseGitIgnore((string)null!));
+    }
+
+    [TestMethod]
+    public void IgnoreRuleSet_ParseGitIgnore_TextReader_ShouldThrowOnNull()
+    {
+        Assert.Throws<ArgumentNullException>(() => IgnoreRuleSet.ParseGitIgnore((TextReader)null!));
+    }
+
+    [TestMethod]
+    public void IgnoreRuleSet_ParseGitIgnore_Stream_ShouldThrowOnNull()
+    {
+        Assert.Throws<ArgumentNullException>(() => IgnoreRuleSet.ParseGitIgnore((Stream)null!));
+    }
+
+    [TestMethod]
+    public void IgnoreRuleSet_Parse_String_ShouldThrowOnNull()
+    {
+        Assert.Throws<ArgumentNullException>(() => IgnoreRuleSet.Parse((string)null!, IgnoreDialect.GitIgnore));
+    }
+
+    [TestMethod]
+    public void IgnoreRuleSet_Parse_TextReader_ShouldThrowOnNull()
+    {
+        Assert.Throws<ArgumentNullException>(() => IgnoreRuleSet.Parse((TextReader)null!, IgnoreDialect.GitIgnore));
+    }
+
+    [TestMethod]
+    public void IgnoreRuleSet_Parse_Stream_ShouldThrowOnNull()
+    {
+        Assert.Throws<ArgumentNullException>(() => IgnoreRuleSet.Parse((Stream)null!, IgnoreDialect.GitIgnore));
+    }
+
+    [TestMethod]
+    public void IgnoreRuleSet_ParseIgnoreFile_ShouldThrowOnNull()
+    {
+        Assert.Throws<ArgumentNullException>(() => IgnoreRuleSet.ParseIgnoreFile(null!));
+    }
+
+    [TestMethod]
+    public void IgnoreMatcher_Evaluate_ShouldThrowOnNull()
+    {
+        var matcher = new IgnoreMatcher(IgnoreRuleSet.ParseGitIgnore("*.tmp"));
+        Assert.Throws<ArgumentNullException>(() => matcher.Evaluate((string)null!));
     }
 
     [TestMethod]
@@ -34,6 +90,12 @@ public class PublicApiGuardTests
     public void RepositoryDiscovery_Discover_ShouldThrowOnNull()
     {
         Assert.Throws<ArgumentNullException>(() => RepositoryDiscovery.Discover(null!));
+    }
+
+    [TestMethod]
+    public void RepositoryDiscovery_TryDiscover_ShouldThrowOnNull()
+    {
+        Assert.Throws<ArgumentNullException>(() => RepositoryDiscovery.TryDiscover(null!, out _));
     }
 
     [TestMethod]
